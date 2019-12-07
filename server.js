8c381455081c43cfded633d60959691b7f829f57
Steven Miyauchi <swm9220@gmail.com>
2019-12-06 17:21:58 -0800

    server

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+app.use(express.static('./public'));
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`listening on ${PORT}`);
+});
+
+// app.get('/', (request, response) => {
+//     response.send('Hello from the back side');
+// })
